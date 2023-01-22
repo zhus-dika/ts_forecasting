671f563a562f4dd182356a1efaf344cdd75253d6
dinara <56104791+zhus-dika@users.noreply.github.com>
2023-01-22 11:18:41 +0600

    add utils.py file

---
+++ utils.py
@@ -0,0 +1,394 @@
+import os
+import numpy as np
+import math
+import scipy as sc
+import matplotlib.pyplot as plt
+
+# Initialized Exponential Smoothing
+# x <array Tx1>- pandas time series, 
+# h <scalar> - forecasting delay
+# Params <dict> - dictionary with 
+#    alpha <scalar in [0,1]> - smoothing parameter
+#    AdaptationPeriod <int> - length of Adaptation period in timestamps
+
+def InitExponentialSmoothing(x, h, Params):
+    T = len(x)
+    alpha = Params['alpha']
+    AdaptationPeriod=Params['AdaptationPeriod']
+    FORECAST = [np.NaN]*(T+h)
+    if alpha>1:
+        w.warn('Alpha can not be more than 1')
+        #alpha = 1
+        return FORECAST
+    if alpha<0:
+        w.warn('Alpha can not be less than 0')
+        #alpha = 0
+        return FORECAST
+    y = x[0]
+    t0=0
+    for t in range(0, T):
+        if not math.isnan(x[t]):
+            if math.isnan(y):
+                y=x[t]
+                t0=t
+            if (t-t0+1)<AdaptationPeriod:
+                y = y*(1-alpha*(t-t0+1)/(AdaptationPeriod)) + alpha*(t-t0+1)/(AdaptationPeriod)*x[t]
+            y = y*(1-alpha) + alpha*x[t]
+            #else do not nothing
+        FORECAST[t+h] = y
+    return FORECAST	
+
+# Simple Exponential Smoothing
+# x <array Tx1>- time series, 
+# h <scalar> - forecasting delay
+# Params <dict> - dictionary with 
+#    alpha <scalar in [0,1]> - smoothing parameter
+def SimpleExponentialSmoothing(x, h=1, Params={}):
+    T = len(x)
+    alpha = Params['alpha']
+    FORECAST = [np.NaN]*(T+h)
+    if alpha>1:
+        w.warn('Alpha can not be more than 1')
+        #alpha = 1
+        return FORECAST
+    if alpha<0:
+        w.warn('Alpha can not be less than 0')
+        #alpha = 0
+        return FORECAST
+    # initialization
+    y = x[0]
+    for cntr in range(T):
+        if not math.isnan(x[cntr]):
+            if math.isnan(y):
+                y=x[cntr]
+            y = alpha*x[cntr] + (1-alpha)*y  # = y + alpha*(x[cntr]-y) 
+            #else do not nothing
+        FORECAST[cntr+h] = y
+    return FORECAST
+
+def HoltExponentialSmoothing(x, h, Params):
+    T = len(x)
+    alpha = Params['alpha']
+    beta = Params['beta']
+    AdaptationPeriod = Params['AdaptationPeriod']
+    
+    FORECAST = [np.NaN]*(T+h)
+    if alpha>1:
+        w.warn('Alpha can not be more than 1')
+        #alpha = 1
+        return FORECAST
+    if alpha<0:
+        w.warn('Alpha can not be less than 0')
+        #alpha = 0
+        return FORECAST
+    if beta>1:
+        w.warn('beta can not be more than 1')
+        #beta = 1
+        return FORECAST
+    if beta<0:
+        w.warn('beta can not be less than 0')
+        #beta = 0
+        return FORECAST
+    
+    
+    l= np.NaN
+    b= np.NaN
+    
+    for t in range(T):
+        if not math.isnan(x[t]):
+            if math.isnan(l):
+                l = x[t]
+                b = x[t+1]-x[t]
+                t0 = np.NaN
+                
+            l_prev = l    
+                        
+            if (t-t0+1)<AdaptationPeriod:
+                l = (1-(1-alpha)*(t-t0+1)/(AdaptationPeriod))* x[t] + (1-alpha)*(t-t0+1)/(AdaptationPeriod)*(l+b)
+                b = (1-beta)*(t-t0+1)/(AdaptationPeriod)*(l - l_prev) + (1-alpha)*(t-t0+1)/(AdaptationPeriod)*b
+            else:
+                l = alpha* x[t] + (1-alpha)*(l+b)
+                b = beta* (l - l_prev) + (1- beta)*b
+                # b = beta* (x[t] - l_prev) + (1- beta)*b
+
+
+        FORECAST[t+h] = l+ b*h
+    return FORECAST
+	
+def AdditiveWintersExponentialSmoothing(x, h, Params):
+    T = len(x)
+    alpha = Params['alpha']
+    gamma = Params['gamma']
+    p = Params['seasonality_period']
+    
+    FORECAST = [np.NaN]*(T+h)
+    
+    l= np.NaN
+    s= []
+    
+    for cntr in range(T):
+        if not math.isnan(x[cntr]):
+            if math.isnan(l):
+                l= x[cntr]
+            if len(s)==0:
+                # looking in the future
+                for i in range(p):
+                    s.append(x[i])
+            if cntr<p:
+                l = alpha*(x[cntr]-s[cntr])+(1-alpha)*l # recurrent smoothing of level 
+            else:
+                s.append(gamma*(x[cntr]-l)+(1-gamma)*s[cntr-p])
+                l = alpha*(x[cntr]-s[cntr-p])+(1-alpha)*l # recurrent smoothing of level 
+                
+        FORECAST[cntr+h] = l + s[cntr+h-(1+h//p)*p]
+    return FORECAST
+
+
+def TheilWageExponentialSmoothing(x, h, Params):
+    T = len(x)
+    alpha = Params['alpha']
+    beta = Params['beta']
+    gamma = Params['gamma']
+    p = Params['seasonality_period']
+    
+    FORECAST = [np.NaN]*(T+h)
+    
+    l= np.NaN
+    b=np.NaN
+    s= []
+    
+    for cntr in range(T):
+        if not math.isnan(x[cntr]):
+            if math.isnan(l):
+                l= x[cntr]
+            if math.isnan(b):
+                b= 0
+            
+            if len(s)==0:
+                for i in range(p):
+                    s.append(x[i])
+                    
+                    
+            if cntr<p:
+                l_old=l
+                l = alpha*(x[cntr]-s[cntr])+(1-alpha)*(l+b)
+                b=beta*(l-l_old)+(1-beta)*b
+            else:
+                l_old=l
+                s.append(gamma*(x[cntr]-l)+(1-gamma)*s[cntr-p])
+                l = alpha*(x[cntr]-s[cntr-p])+(1-alpha)*(l+b) # recurrent smoothing of level 
+                b=beta*(l-l_old)+(1-beta)*b
+            
+        FORECAST[cntr+h] = l+b + s[cntr+h - (1+h//p)*p]
+    return FORECAST
+	
+# AdaptiveExponentialSmoothing
+# x <array Tx1>- time series, 
+# h <scalar> - forecasting delay
+# Params <dict> - dictionary with 
+#    alpha <scalar in [0,1]> - smoothing parameter
+#    AdaptivePeriod scalar> - adapation period for initialization
+#    gamma<scalar in [0,1]> - parametr of cross validation
+def AdaptiveExponentialSmoothing(x, h, Params):
+    T = len(x)
+    alpha = Params['alpha']
+    gamma = Params['gamma']
+    AdaptationPeriod=Params['AdaptationPeriod']
+    FORECAST = [np.NaN]*(T+h)
+    if alpha>1:
+        w.warn('Alpha can not be more than 1')
+        #alpha = 1
+        return FORECAST
+    if alpha<0:
+        w.warn('Alpha can not be less than 0')
+        #alpha = 0
+        return FORECAST
+    y = np.NaN
+    t0= np.NaN
+    e1= np.NaN
+    e2= np.NaN
+    Kt_1 = alpha
+    K=alpha
+    for t in range(0, T):
+        if not math.isnan(x[t]):
+            if math.isnan(y):
+                y=x[t]
+                t0=t
+                e1=alpha
+                e2 = 1
+            else:
+                if (t-t0)<h:
+                    e1 = gamma*(x[t]-y)+(1-gamma)*e1
+                    e2 = gamma*np.abs(x[t]-y)+(1-gamma)*e2
+                else:
+                    e1 = gamma*(x[t]-FORECAST[t])+(1-gamma)*e1
+                    e2 = gamma*np.abs(x[t]-FORECAST[t])+(1-gamma)*e2
+            
+            if e2==0:
+                K=alpha
+            else:
+                K=np.abs(e1/e2)
+
+            alpha=Kt_1
+            Kt_1=K
+
+            if (t-t0+1)<AdaptationPeriod:
+                y = y*(1-alpha)*(t-t0+1)/(AdaptationPeriod) + (1-(1-alpha)*(t-t0+1)/(AdaptationPeriod))*x[t]
+            else:
+                y = y*(1-alpha) + (alpha)*x[t]
+        FORECAST[t+h] = y
+    return FORECAST
+
+# generate forecast values based on particular algorithm
+# h - forecast horizon, each point in historical period will be forecasted with delay = h (h-step ahead)
+# ts - <pandas data frame> with timestamps in index, each column contains particular timeseries, all of them will be forecasted independently
+# AlgName - <str> name of the function that runs forecasting algorithm 
+# AlgTitle <str> - a name of the forecasting algorithm
+# step <char> - aggregation method of the original data before forecasting
+# ParamsArray <array> - array of parameter set, each component of array defines particular forecasting algorithm
+def build_forecast(h, ts, alg_name, alg_title, params_array, step='D'):
+      
+	FRC_TS = dict()
+
+	for p in params_array:
+		frc_horizon = pd.date_range(ts.index[-1], periods=h+1, freq=step)[1:]
+		frc_ts = pd.DataFrame(index = ts.index.append(frc_horizon), columns = ts.columns)
+	  
+		for cntr in ts.columns:
+			frc_ts[cntr] = eval(alg_name)(ts[cntr], h, p)
+
+		#         frc_ts.columns = frc_ts.columns+('%s %s' % (alg_title, p))
+		FRC_TS['%s %s' % (alg_title, p)] = frc_ts
+
+	return FRC_TS
+
+# draw forecast and original time series
+# ts - <pandas data frame> with timestamps in index, each column contains particular timeseries
+# frc_ts - <pandas data frame> the same structure as ts, 
+# ts_num <int> - column index for which plot shoud be drawn
+# alg_title <str> - a name of the forecasting algorithm
+def plot_ts_forecast(ts, frc_ts, ts_num=0, alg_title=''):
+	frc_ts.columns = ts.columns+'; '+alg_title
+	ts[ts.columns[ts_num]].plot(style='b', linewidth=1.0, marker='o')
+	ax = frc_ts[frc_ts.columns[ts_num]].plot(style='r-^', figsize=(25,5), linewidth=1.0)
+	plt.xlabel("Time ticks")
+	plt.ylabel("TS values")
+	plt.legend()
+	return ax
+
+# Quality functions
+def qualitySSE(x,y):
+    # Sum squared error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    return ((x-y)**2).sum(), (x-y)**2
+
+def qualityMSE(x,y):
+    # Mean squared error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    return ((x-y)**2).mean() , (x-y)**2
+
+def qualityRMSE(x,y):
+    # Root mean squared error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    return (((x-y)**2).mean())**(0.5) , (x-y).abs()
+
+def qualityMAE(x,y):
+    # Mean absolute error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    return (x-y).abs().mean(), (x-y).abs()
+
+def qualityMAPE(x,y):
+    # Mean absolute percentage error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    qlt = ((x-y).abs()/x.abs()).replace([np.inf, -np.inf], np.nan)
+    return qlt.mean() , qlt
+
+def qualityMAPPE(x,y):
+    # Mean absolute predicted percentage error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    qlt = ((x-y).abs()/y.abs()).replace([np.inf, -np.inf], np.nan)
+    return qlt.mean() , qlt
+
+def qualitySMAPE(x,y):
+    # Symmetric mean absolute percentage error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    qlt = (2*(x-y).abs()/(x+y)).replace([np.inf, -np.inf], np.nan)
+    return qlt.mean() , qlt
+
+def qualityMAMAXPE(x,y):
+    # Mean absolute maximum percentage error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    qlt = ((x-y).abs()/max(x.abs(), y.max())).replace([np.inf, -np.inf], np.nan)
+    return qlt.mean() , qlt
+
+def qualityMASE(x,y, init_step=0):
+    # Mean absolute scaled error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    qlt = ((x-y).abs()/(x-x.shift(1)).abs())[init_step:].replace([np.inf, -np.inf], np.nan)
+    return qlt.mean() , qlt
+
+def qualityMedianAE(x,y):
+    # Median absolute error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    return ((x-y).abs()).median(), (x-y).abs()
+
+def qualityWAPE(x,y):
+    # Weighted absolute percentage error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    denom = x.abs().sum()
+    qlt = ((x-y).abs()/denom).replace([np.inf, -np.inf], np.nan)
+    return qlt.sum() , qlt
+
+def qualityWAPPE(x,y):
+    # Weighted absolute percentage error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    denom = y.abs().sum()
+    qlt = ((x-y).abs()/denom).replace([np.inf, -np.inf], np.nan)
+    return qlt.sum() , qlt
+
+def qualityWAMAXPE(x,y):
+    # Weighted absolute maximum percentage error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    denom = pd.merge(x, y, right_index = True, left_index = True).max(axis = 1).sum()
+    qlt = ((x-y).abs()/denom).replace([np.inf, -np.inf], np.nan)
+    return qlt.sum() , qlt
+
+def qualityMedianAE(x,y):
+    # Median absolute error
+    # x,y - pandas structures
+    # x - real values
+    # y - forecasts
+    return ((x-y).abs()).median(), (x-y).abs()
+	
+def get_autoregrmatrix(x,h,K):
+    T = len(x)
+    X = sc.linalg.hankel(x[:T-h-K+1], 
+                          np.hstack((x[T-h-K:T-h]))) # is needed to repeat x[-K] in second part
+    y = x[K+h-1:]
+    return X,y
